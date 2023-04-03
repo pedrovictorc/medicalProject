baadd22a50aa6cd6a38ccbbbad80101d14ae1050
Pedro Victor Carvalho <pedrocarvalhophb@gmail.com>
2023-04-03 15:49:06 -0300

    First comit at this project.

---
+++ src/main/java/com/medical/projectmedical/ProjectMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.medical.projectmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMedicalApplication.class, args);
+    }
+
+}
